69631bcd78d1eb701e17c4983e3008360bdfa51b
tukhoi <ntkhoi@live.com>
2014-06-10 16:15:14 +0700

    v1.2.1

--- DaVang.Utilities/Extensions/PhoneApplicationPageExtensions.cs
+++ DaVang.Utilities/Extensions/PhoneApplicationPageExtensions.cs
@@ -1,47 +1,47 @@
-﻿using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
+﻿//using Microsoft.Phone.Controls;
+//using Microsoft.Phone.Shell;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows.Media;
+//using System.Windows.Media.Imaging;
 
-namespace Davang.Utilities.Extensions
-{
-    public static class PhoneApplicationPageExtensions
-    {
-        public static void SetProgressIndicator(this PhoneApplicationPage page, bool isVisible, string message = "")
-        {
-            if (SystemTray.ProgressIndicator == null)
-                SystemTray.ProgressIndicator = new ProgressIndicator();
+//namespace Davang.Utilities.Extensions
+//{
+//    public static class PhoneApplicationPageExtensions
+//    {
+//        public static void SetProgressIndicator(this PhoneApplicationPage page, bool isVisible, string message = "")
+//        {
+//            if (SystemTray.ProgressIndicator == null)
+//                SystemTray.ProgressIndicator = new ProgressIndicator();
 
-            SystemTray.ProgressIndicator.IsIndeterminate = isVisible;
-            SystemTray.ProgressIndicator.IsVisible = isVisible;
-            SystemTray.ProgressIndicator.Text = message;
-        }
+//            SystemTray.ProgressIndicator.IsIndeterminate = isVisible;
+//            SystemTray.ProgressIndicator.IsVisible = isVisible;
+//            SystemTray.ProgressIndicator.Text = message;
+//        }
 
-        public static void BackToMainPage(this PhoneApplicationPage page)
-        {
-            var mainPageUri = "/MainPage.xaml";
+//        public static void BackToMainPage(this PhoneApplicationPage page)
+//        {
+//            var mainPageUri = "/MainPage.xaml";
 
-            string terms = page.NavigationContext.QueryString.GetQueryString("terms");
-            mainPageUri += !string.IsNullOrEmpty(terms) ? "?terms=" + terms : string.Empty;
+//            string terms = page.NavigationContext.QueryString.GetQueryString("terms");
+//            mainPageUri += !string.IsNullOrEmpty(terms) ? "?terms=" + terms : string.Empty;
 
-            page.NavigationService.Navigate(new Uri(mainPageUri, UriKind.Relative));
-        }
+//            page.NavigationService.Navigate(new Uri(mainPageUri, UriKind.Relative));
+//        }
 
-        public static void BackToPreviousPage(this PhoneApplicationPage page)
-        {
-            page.NavigationService.GoBack();
-        }
+//        public static void BackToPreviousPage(this PhoneApplicationPage page)
+//        {
+//            page.NavigationService.GoBack();
+//        }
 
-        public static void SetBackground(this PhoneApplicationPage page, Uri backgroundImageUri)
-        {
-            var background = new ImageBrush();
-            background.ImageSource = new BitmapImage(backgroundImageUri);
-            page.Background = background;
-        }
-    }
-}
+//        public static void SetBackground(this PhoneApplicationPage page, Uri backgroundImageUri)
+//        {
+//            var background = new ImageBrush();
+//            background.ImageSource = new BitmapImage(backgroundImageUri);
+//            page.Background = background;
+//        }
+//    }
+//}
