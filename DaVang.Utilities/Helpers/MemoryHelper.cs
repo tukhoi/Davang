f4cd78c3f2cd9a48ef0e75c205c1b76beb39f414
tukhoi <ntkhoi@live.com>
2014-07-23 17:12:38 +0700

    v1.7.2

---
+++ DaVang.Utilities/Helpers/MemoryHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Phone.Info;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Davang.Utilities.Helpers
+{
+    public static class MemoryHelper
+    {
+        public static bool IsLowMemDevice { get; set; }
+
+        static MemoryHelper()
+        {
+            try
+            {
+                Int64 result = (Int64)DeviceExtendedProperties.GetValue("ApplicationWorkingSetLimit");
+                var resultInMb = result / (1024 * 1024);
+                IsLowMemDevice = resultInMb < 200;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                //windows phone OS not updated, then 512mb
+                IsLowMemDevice = true;
+            }
+        }
+
+    }
+}
